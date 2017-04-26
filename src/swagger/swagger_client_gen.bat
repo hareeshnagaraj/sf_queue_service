579e13e540ee177b20bc89f42ff1df37ed8cd103
Hareesh Nagaraj <hanagar@microsoft.com>
2017-04-26 14:54:20 -0700

    Add new batch file, codegen jar

---
+++ src/swagger/swagger_client_gen.bat
@@ -0,0 +1,4 @@
+java -jar swagger-codegen-cli-2.2.1.jar generate -i swagger.yaml -l java -o gen/client/java
+java -jar swagger-codegen-cli-2.2.1.jar generate -i swagger.yaml -l javascript -o gen/client/javascript
+java -jar swagger-codegen-cli-2.2.1.jar generate -i swagger.yaml -l python -o gen/client/python
+java -jar swagger-codegen-cli-2.2.1.jar generate -i swagger.yaml -l csharp -o gen/client/csharp
