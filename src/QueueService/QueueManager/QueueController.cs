13a4cf3c270815c2d09dce141ee2f77d14830ab9
Tyler Adams <tyadam@microsoft.com>
2017-04-27 15:47:54 -0700

    wip controller

---
+++ src/QueueService/QueueManager/QueueController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QueueManager.Controllers
+{
+    [Route("api/[controller]")]
+    class QueueController : Controller
+    {
+        [HttpPost("{name}")]
+        public string GetOrCreateQueue(string name)
+        {
+            return "test";
+        }
+    }
+}
